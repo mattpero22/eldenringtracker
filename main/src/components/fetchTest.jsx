e010d60e20bf6b85f8b42d0f658c0b5d169f78b9
matt pero <mattpero22@gmail.com>
2023-03-02 09:30:14 -0500

    reviewed code for last session and beginning wireframe/basic layout of landing page]

--- main/src/components/fetchTest.jsx
+++ main/src/components/fetchTest.jsx
@@ -9,26 +9,26 @@ const FetchTest = (props) => {
     // sets states from the api call result
     // items initialzed with [] such that useEffect will run once
     useEffect(() => {
-        fetch("https://eldenring.fanapis.com/api/items?limit=2")
-            .then(res => res.json())
+        fetch("https://eldenring.fanapis.com/api/items?limit=2")    // makes api call
+            .then(res => res.json())                                // parses completed promise data as json
             .then(
-                (result) => {
+                (result) => {                             // load all data into state of items
                     setIsLoaded(true)
                     setItems(result)
                 },
-                (error) => {
+                (error) => {                              // set state of error to the error msg
                     setIsLoaded(true)
                     setError(error)
                 }
             )
-    }, [])
+    }, [])  // this useEffect is not dependent on state, for the purpose of this example should run every time when component is present in APP
 
     if (error) {
-        return <div>Error: {error.message}</div>
+        return <div>Error: {error.message}</div>        // error DOM elements
     } else if (!isLoaded) {
-        return <div>Loading...</div>
-    } else {
-        return <div>Success!
+        return <div>Loading...</div>                    // pending/loading endpoint elements
+    } else {                                                
+        return <div>                                                               
             <>{items.data[0].name}</>
             <>{items.data[1].name}</>
         </div>
